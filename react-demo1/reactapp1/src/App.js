2035040ec11c36cbb71fea8d409b1f29a1939976
DDXY1230 <2502752288@qq.com>
2020-09-28 16:37:42 +0800

    零散知识点

--- react-demo1/reactapp1/src/App.js
+++ react-demo1/reactapp1/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react'
+import FriendList from './FriendList'
+
+class App extends Component {
+	/**
+	 * 类组件：
+	 * 1.必须继承自React.Component
+	 * 2.必须有一个render方法
+	 * 3.render中定义要输出的组件
+	 */
+	render() {
+		return (
+			<div className="box">
+				<h1>这是一个React</h1>
+				<FriendList/>
+			</div>
+		)
+	}
 }
-
-export default App;
+export default App
