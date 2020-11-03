4e4d5fa5f0a47fd3db2beba993cf57df2fea0e6a
DDXY1230 <2502752288@qq.com>
2020-11-03 10:07:32 +0800

    用symbol

--- notebook/index.js
+++ notebook/index.js
@@ -5,3 +5,14 @@ console.log('112233')
 
 const r = Symbol()
 console.log(s === r)// Symbol()永远是一个独一无二的值
+
+console.log(Symbol('foo'))
+//
+const name = Symbol()
+const person = {
+    [name]: 'zce',
+    say() {
+        console.log(this[name])// 在这个对象的内部可以通过创建属性时的名字拿到,而在外边无法创建一抹一样的symbol,所以外部无法取到,symbol最突出的作用就是为对象创建私有属性
+    }
+}
+person.say()
