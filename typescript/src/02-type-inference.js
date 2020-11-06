6838ad07bbcde62ea10f94cc1e3e2f14a304360c
DDXY1230 <2502752288@qq.com>
2020-11-06 17:05:39 +0800

    一些小demo

---
+++ typescript/src/02-type-inference.js
@@ -0,0 +1,16 @@
+const { forOwn } = require("lodash")
+
+//@flow
+function square(n) {
+    return n*n
+}
+square(100)
+let num: number = 100
+return for():number {
+
+}
+const b: number = Infinity //NaN // 10
+const c: boolean = true //false
+const e: null = null
+const f: void = undefined
+const h: symbol = Symbol()
