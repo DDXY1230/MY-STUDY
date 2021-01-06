96a32284cce762010c9fdcee60a76c76208f2992
DDXY1230 <2502752288@qq.com>
2021-01-06 18:26:32 +0800

    类型判断

--- 常见面试题原理解析/typeof和instanceof的区别.js
+++ 常见面试题原理解析/typeof和instanceof的区别.js
@@ -10,6 +10,7 @@ console.log(Object.prototype.toString.call(new RegExp('a')))// [object RegExp]
 console.log(Object.prototype.toString.call('hhh'))// [object String]
 
 // instanceof
+// 但是instanceof不能处理基本数据类型,只能处理构造函数创建出来的实例
 console.log([] instanceof Array)
 console.log({} instanceof Object)
 class A {
@@ -30,3 +31,22 @@ function instanceof_ (A,B) {
   }
 }
 console.log(instanceof_(a,A))
+
+//--------------------------
+console.log('============')
+function func1() {
+  // arguments.__proto__ = Array.prototype  // 如果手动修改原型链的指向,那么下面返回true
+  console.log(arguments instanceof Array) // false
+  // arguments 不是一个数组,只是类数组
+}
+func1()
+// --------------------------------
+// constructor
+/**
+ * 不会往上找,constructor的属性太容易被修改
+ */
+let n = 12
+console.log(n.constructor === Number) // true
+let a22 = []
+console.log(a22.constructor === Array) // true
+console.log(a22.constructor === Object) // false
