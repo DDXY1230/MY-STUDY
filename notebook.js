735af445d94fe848bd2cb7a31e51ba1089802895
DDXY1230 <2502752288@qq.com>
2021-02-18 23:32:02 +0800

    some points

--- notebook.js
+++ notebook.js
@@ -1,6 +1,15 @@
-let url = require('url')
-let str = 'http://user:pwd@localhost:8080/user?id=5#top'
-//第二个参数为true,可以吧query解析成对象
-let urlObj = url.parse(str,true)
-console.log(urlObj)
-console.log(url.format(urlObj))
+// let url = require('url')
+// let str = 'http://user:pwd@localhost:8080/user?id=5#top'
+// //第二个参数为true,可以吧query解析成对象
+// let urlObj = url.parse(str,true)
+// console.log(urlObj)
+// console.log(url.format(urlObj))
+
+console.log('梁伟业吃粑粑', process.platform)
+
+let config = process.argv.slice(2).reduce((memo,current,index,array) => {
+  if(current.includes('--')){
+    memo[current.slice(2)] = array[index + 1]
+  }
+  return memo
+}, {})
